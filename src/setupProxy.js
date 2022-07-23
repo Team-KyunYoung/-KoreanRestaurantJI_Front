0ac5e3a6f5b47263ed7c14b2d840aadb1ce264ba
SShinMJ <winny0678@gmail.com>
2022-07-23 20:12:17 +0900

    Fix: Proxy setting

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// 해당 어플리케이션에서 /api 로 시작되는 endpoint 가 서버에 어떠한 통신을 요청한 경우 
+// proxy 서버가 중계 역할을 하며 target으로 지정한 {통신할 서버 주소} 로 통신하면서 서버를 우회함을 의미한다.
+module.exports = function(app) {
+  app.use(
+  '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
+};
