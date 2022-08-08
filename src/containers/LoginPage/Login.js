199be17741dcafefef339df3f446ae3be0b62806
1g2g <wonji126326@gmail.com>
2022-08-08 14:37:32 +0900

    fix login and signup, add updatePassword

--- src/containers/LoginPage/Login.js
+++ src/containers/LoginPage/Login.js
@@ -1,120 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import styles from './Login.module.scss';
-import Header from 'components/header/Header';
-import Footer from 'components/footer/Footer';
-import Authentication from 'lib/api/Authentication';
-import axios from "axios";
+import styles from "./Login.module.scss";
+import Header from "components/header/Header";
+import Footer from "components/footer/Footer";
+import * as Authentication from "lib/api/Authentication";
 
 //
 const Login = () => {
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    const [token, setToken] = useState(localStorage.getItem("token") || '');
-    const [hasLoginFailed, setHasLoginFailed] = useState(false);
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+  const [hasLoginFailed, setHasLoginFailed] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-    const EmailHandleChange = (e) => setUserEmail(e.target.value);
-    const passwordHandleChange = (e) => setUserPassword(e.target.value);
+  const EmailHandleChange = (e) => setUserEmail(e.target.value);
+  const passwordHandleChange = (e) => setUserPassword(e.target.value);
 
-    const loginClicked = () => {
-        console.log('loginClicked')
-        console.log(userEmail)
-        Authentication
-            .login(userEmail, userPassword)
-            .then((response) => {
-                console.log(response)
-                // setToken();
-                document.location.href = "/";
-                setShowSuccessMessage(true);
-                setHasLoginFailed(false);
-            }).catch(() => {
-                // console.log(error.response)
-                setShowSuccessMessage(false);
-                setHasLoginFailed(true);
-                alert('Login Failed');
-            });
-    }
-    // 페이지 렌더링 후 가장 처음 호출되는 함수
-    {/* useEffect(() => {
+  const loginClicked = () => {
+    console.log("loginClicked");
+    console.log(userEmail);
+    Authentication.login(userEmail, userPassword)
+      .then((response) => {
+        console.log(response);
+        // setToken();
+        document.location.href = "/";
+        setShowSuccessMessage(true);
+        setHasLoginFailed(false);
+      })
+      .catch(() => {
+        // console.log(error.response)
+        setShowSuccessMessage(false);
+        setHasLoginFailed(true);
+        alert("Login Failed");
+      });
+  };
+  // 페이지 렌더링 후 가장 처음 호출되는 함수
+  {
+    /* useEffect(() => {
         axios.get('/login')
             .then(res => console.log(res))
             .catch()
     },
         // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
         [])
-*/}
-    const socialLoginGoogle = () => {
-        console.log("google login clicked")
-        //Authentication.loginSocialGoogle();
-    }
+*/
+  }
+  const socialLoginGoogle = () => {
+    console.log("google login clicked");
+    //Authentication.loginSocialGoogle();
+  };
 
-    const socialLoginKakao = () => {
-        console.log("kakao login clicked")
-        //Authentication.loginSocialKakao();
-    }
+  const socialLoginKakao = () => {
+    console.log("kakao login clicked");
+    //Authentication.loginSocialKakao();
+  };
 
-    return (
-        <div className="loginPage">
-            <Header />``
-            <main>
-                <div className={styles.content}>
-                    <header className={styles.title}>
-                        <h1 id='title'>로그인</h1>
-                    </header>
-                    <section className={styles.main}>
-                        <form >
-                            <div className={styles.inputArea}>
-                                {hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
-                                {showSuccessMessage && <div>Login Sucessful</div>}
-                                <div className={styles.inputId}>
-                                    <input id="userEmail" name="userEmail" placeholder="이메일을 입력하세요" type="email" onChange={EmailHandleChange} />
-                                </div>
-                                <div className={styles.inputPw}>
-                                    <input id="userPassword" name="userPassword" placeholder="비밀번호" type="password" autoComplete="on" onChange={passwordHandleChange} />
-                                </div>
-                            </div>
-                        </form>
-
-                        <div className={styles.loginBtn}>
-                            <div>
-                                <button id="login" onClick={loginClicked}>로그인</button>
-                            </div>
-                        </div>
-
-
-                        <div className={styles.social}>
-                            <div>소셜로그인</div>
-                            <hr></hr>
-                            <div className={styles.btnCarrier}>
-                                <div className={styles.socialBtn}>
-                                    <img src={require('assets/btn/btn_google_signin.png')} onClick={socialLoginGoogle} alt="google signin" />
-                                </div>
-                                <div className={styles.socialBtn}>
-                                    <img src={require('assets/btn/btn_kakao_signin.png')} onClick={socialLoginKakao} alt="kakao signin" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={styles.userAccess}>
-                            <div className={styles.signupBtn}>
-                                <Link to='/signup'>회원가입</Link>
-                            </div>
-                            <div className={styles.signupBtn}>
-                                <Link to='/signup'>아이디 찾기</Link>
-                            </div>
-                            <div className={styles.signupBtn}>
-                                <Link to='/signup'>비밀번호 찾기</Link>
-                            </div>
-                        </div>
-
-                    </section>
+  return (
+    <div className="loginPage">
+      <Header />
+      ``
+      <main>
+        <div className={styles.content}>
+          <header className={styles.title}>
+            <h1 id="title">로그인</h1>
+          </header>
+          <section className={styles.main}>
+            <form>
+              <div className={styles.inputArea}>
+                {hasLoginFailed && (
+                  <div className="alert alert-warning">Invalid Credentials</div>
+                )}
+                {showSuccessMessage && <div>Login Sucessful</div>}
+                <div className={styles.inputId}>
+                  <input
+                    id="userEmail"
+                    name="userEmail"
+                    placeholder="이메일을 입력하세요"
+                    type="email"
+                    onChange={EmailHandleChange}
+                  />
                 </div>
-            </main>
-            <Footer />
+                <div className={styles.inputPw}>
+                  <input
+                    id="userPassword"
+                    name="userPassword"
+                    placeholder="비밀번호"
+                    type="password"
+                    autoComplete="on"
+                    onChange={passwordHandleChange}
+                  />
+                </div>
+              </div>
+            </form>
+
+            <div className={styles.loginBtn}>
+              <div>
+                <button id="login" onClick={loginClicked}>
+                  로그인
+                </button>
+              </div>
+            </div>
+
+            <div className={styles.social}>
+              <div>소셜로그인</div>
+              <hr></hr>
+              <div className={styles.btnCarrier}>
+                <div className={styles.socialBtn}>
+                  <img
+                    src={require("assets/btn/btn_google_signin.png")}
+                    onClick={socialLoginGoogle}
+                    alt="google signin"
+                  />
+                </div>
+                <div className={styles.socialBtn}>
+                  <img
+                    src={require("assets/btn/btn_kakao_signin.png")}
+                    onClick={socialLoginKakao}
+                    alt="kakao signin"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.userAccess}>
+              <div className={styles.signupBtn}>
+                <Link to="/signup">회원가입</Link>
+              </div>
+              <div className={styles.signupBtn}>
+                <Link to="/findPassword">비밀번호 찾기</Link>
+              </div>
+            </div>
+          </section>
         </div>
-    );
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Login;
--- src/lib/api/Authentication.js
+++ src/lib/api/Authentication.js
@@ -1,46 +1,187 @@
-import axios from "axios";
+const USER_API_BASE_URL = "/api/user";
 
-const USER_API_BASE_URL = "/api/user"
-
-class Authentication {
-//스웨거에서 불러오는 변수 , axios 공부하기
-    signup(userEmail, userNickname, userPassword) {
-      let userData = {
-        userEmail: userEmail,
-        userNickname: userNickname,
-        userPassword: userPassword,
-      }
-      return axios.post(USER_API_BASE_URL + "/signup", JSON.stringify(userData), {
-        headers: {
-          "Content-Type": `application/json`,
-        },
-      });
-    }
-
-    login(userEmail, userPassword) {
-      let data = {
-        userEmail: userEmail,
-        userPassword: userPassword
-      }
-      
-      return axios.post(USER_API_BASE_URL + "/login", JSON.stringify(data), {
-        headers: {
-          "Content-Type": `application/json`,
-        },
-      })
-    }
-
-//     checkNickname(userNickname){
-//       console.log('name: '+userNickname)
-//       axios.get('/api/user/checkNickname').then((response)=>{
-// console.log(response.data);
-// }).catch((error)=>{
-// console.log(error);
-// })
-  
-//     }
+export function signup(userEmail, userNickname, userPassword) {
+  let data = {
+    userEmail: userEmail,
+    userNickname: userNickname,
+    userPassword: userPassword,
+  };
+  const signupInfo = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/signup", signupInfo);
 }
-//delte추가해보기 -> 마이페이지에서 버튼 만들어서 공부
-//아이디 비번 찾기
 
-export default new Authentication()
+export function login(userEmail, userPassword) {
+  let data = {
+    userEmail: userEmail,
+    userPassword: userPassword,
+  };
+  const loginInfo = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/login", loginInfo);
+}
+export function emailAuth(userEmail) {
+  const data = {
+    userEmail: userEmail,
+  };
+  const emailInfo = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/signup/emailAuth", emailInfo);
+}
+export function checknickname(userNickname) {
+  const data = {
+    userNickname: userNickname,
+  };
+  const nicknameInfo = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/checknickname", nicknameInfo);
+}
+export function emailAuthForPassword(userEmail) {
+  const data = {
+    userEmail: userEmail,
+  };
+  const emailInfo = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/find/emailAuth", emailInfo);
+}
+export function updatePassword(userId, userPassword) {
+  const data = {
+    userId: userId,
+    userPassword: userPassword,
+  };
+  const passwordInfo = {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/update/password", passwordInfo);
+}
+export function updateNickname(userId, userNickname) {
+  const data = {
+    userId: userId,
+    userNickname: userNickname,
+  };
+  const nicknameInfo = {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "/update/nickname", nicknameInfo);
+}
+export function deleteUserByAdmin(userEmail) {
+  const data = {
+    userEmail: userEmail,
+  };
+  const deleteInfo = {
+    method: "DELETE",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": `application/json`,
+    },
+  };
+  return fetch(USER_API_BASE_URL + "delete", deleteInfo);
+}
+export function deleteUserByItself(userEmail) {
+  return fetch(USER_API_BASE_URL + "delete" + userEmail, {
+    method: "DELTE",
+  });
+}
+export function findUserById(userId) {
+  return fetch(USER_API_BASE_URL + "find" + userId, {
+    method: "GET",
+  });
+}
+//아래는 지우지 말기 나중에 axios 만들 때 참고한 뒤 삭제 할 예정
+
+// import axios from "axios";
+
+// const USER_API_BASE_URL = "/api/user";
+
+// class Authentication {
+//   //스웨거에서 불러오는 변수 , axios 공부하기
+//   signup(userEmail, userNickname, userPassword) {
+//     let userData = {
+//       userEmail: userEmail,
+//       userNickname: userNickname,
+//       userPassword: userPassword,
+//     };
+//     return axios.post(USER_API_BASE_URL + "/signup", JSON.stringify(userData), {
+//       headers: {
+//         "Content-Type": `application/json`,
+//       },
+//     });
+//   }
+
+//   login(userEmail, userPassword) {
+//     let data = {
+//       userEmail: userEmail,
+//       userPassword: userPassword,
+//     };
+
+//     return axios.post(USER_API_BASE_URL + "/login", JSON.stringify(data), {
+//       headers: {
+//         "Content-Type": `application/json`,
+//       },
+//     });
+//   }
+// }
+// //delte추가해보기 -> 마이페이지에서 버튼 만들어서 공부
+// //아이디 비번 찾기
+
+// export default new Authentication();
+
+// signup.js
+// Authentication.signup(userEmail, userNickname, userPassword)
+//         .then((response) => {
+//           console.log(response);
+//           alert("회원가입이 완료되었습니다.");
+//           document.location.href = "/login";
+//         })
+//         .catch(() => {
+//           alert("Signup Failed");
+//         });
+
+// login.JS
+// Authentication.login(userEmail, userPassword)
+//   .then((response) => {
+//     console.log(response);
+//     // setToken();
+//     document.location.href = "/";
+//     setShowSuccessMessage(true);
+//     setHasLoginFailed(false);
+//   })
+//   .catch(() => {
+//     // console.log(error.response)
+//     setShowSuccessMessage(false);
+//     setHasLoginFailed(true);
+//     alert("Login Failed");
+//   });
