3ea50f5858da35a08c9b35feb5d3f6c26a99e9fc
1g2g <wonji126326@gmail.com>
2022-08-10 23:46:17 +0900

    add Home page publishing

--- src/containers/HomePage/Home.js
+++ src/containers/HomePage/Home.js
@@ -1,27 +1,41 @@
 import React from "react";
-import './Style.scss';
-import logo from '../../assets/logo.svg';
-//import SubmitBtn from "../../components/<컴포넌트명>";
-import CounterHook from "./CounterHook";//훅
+import styles from "./Home.module.scss";
+import Header from "components/header/Header";
+import Footer from "components/footer/Footer";
+import MainCarousel from "./MainCarousel.js";
+import ParallaxContent from "./ParallaxContent.js";
+
 function Home() {
+  const image1 = "https://picsum.photos/1200/600";
+  const image2 = "https://picsum.photos/1200/500";
+
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>
-          우리 열심히 프로젝트 해보쟈🤤 화이팅!<br/>
-          최선을 다 하겠어..!(〃￣︶￣)人(￣︶￣〃)
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <CounterHook/>
-      </header>
+    <div className="homePage">
+      <Header />
+      <main>
+        <header>
+          <MainCarousel />
+        </header>
+        <ParallaxContent
+          title="Lorem ipsum"
+          contents="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          image={image1}
+          float="right"
+        />
+        <ParallaxContent
+          title="Lorem ipsum"
+          contents="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          image={image2}
+          float="left"
+        />
+        <ParallaxContent
+          title="Lorem ipsum"
+          contents="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          image={image1}
+          float="right"
+        />
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './index.scss';
-import Home from './containers/HomePage/Home';
-import Menu from './containers/MenuPage/Menu';
-import Login from './containers/LoginPage/Login';
-import FindPassword from './containers/LoginPage/FindPassword';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.scss";
+import Home from "./containers/HomePage/Home";
+import Menu from "./containers/MenuPage/Menu";
+import Login from "./containers/LoginPage/Login";
+import FindPassword from "./containers/LoginPage/FindPassword";
 // import Login2 from './containers/LoginPage/Login2';
-import Signup from './containers/SignupPage/Signup';
-import Cart from './containers/CartPage/Cart';
-import Chat from './containers/ChatPage/Chat';
-import Course from './containers/CoursePage/Course';
-import Map from './containers/MapPage/Map';
-import Order from './containers/OrderPage/Order';
-import ReservationDetail from './containers/ReservationDetailPage/ReservationDetail';
-import Reservation from './containers/ReservationPage/Reservation';
-import Review from './containers/ReviewPage/Review';
-import SingleMenu from './containers/SingleMenuPage/SingleMenu';
-import UserInfo from './containers/UserInfoPage/UserInfo';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';//부트스트랩 : 삭제 금지
+import Signup from "./containers/SignupPage/Signup";
+import Cart from "./containers/CartPage/Cart";
+import Chat from "./containers/ChatPage/Chat";
+import Course from "./containers/CoursePage/Course";
+import Map from "./containers/MapPage/Map";
+import Order from "./containers/OrderPage/Order";
+import ReservationDetail from "./containers/ReservationDetailPage/ReservationDetail";
+import Reservation from "./containers/ReservationPage/Reservation";
+import Review from "./containers/ReviewPage/Review";
+import SingleMenu from "./containers/SingleMenuPage/SingleMenu";
+import UserInfo from "./containers/UserInfoPage/UserInfo";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css"; //부트스트랩 : 삭제 금지
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
       <Route path="Menu" element={<Menu />} />
       <Route path="Login" element={<Login />} />
       <Route path="FindPassword" element={<FindPassword />} />
@@ -38,7 +34,7 @@ root.render(
       <Route path="Cart" element={<Cart />} />
       <Route path="Chat" element={<Chat />} />
       <Route path="Course" element={<Course />} />
-      <Route path="Map" element={<Map />} /> 
+      <Route path="Map" element={<Map />} />
       <Route path="Order" element={<Order />} />
       <Route path="ReservationDetail" element={<ReservationDetail />} />
       <Route path="Reservation" element={<Reservation />} />
