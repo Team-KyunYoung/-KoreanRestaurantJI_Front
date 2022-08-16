7331da5a55611117183a16ae9529fee392fab6b3
SShinMJ <winny0678@gmail.com>
2022-08-16 16:25:23 +0900

    Feat: Naver Map Api for Mappage

--- src/containers/MapPage/Map.js
+++ src/containers/MapPage/Map.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
+
+import { FaMapMarkerAlt, FaPhoneAlt, FaBus } from "react-icons/fa";
 
 import styles from "./Map.module.scss";
 import Header from "components/header/Header";
 import Footer from "components/footer/Footer";
 
 const Map = () => {
-  const [token, setToken] = useState(localStorage.getItem("token") || "");
 
   return (
     <div className="MapPage">
@@ -18,7 +19,34 @@ const Map = () => {
 
           </header>
           <section className={styles.main}>
-
+            <div className={styles.contents}>
+              <div className={styles.description}>
+                <h2>오시는 길</h2>
+                <span><FaMapMarkerAlt /> '智'의 실제 주소를 표기합니다.</span><br/>
+                <span><FaPhoneAlt /> '智'의 실제 대표 전화번호를 표기합니다.</span><br/>
+                <span><FaBus /> (ex) 지하철 8호선, 330, 440, 3012, ... </span>
+              </div>
+              <div className={styles.map}>
+                <RenderAfterNavermapsLoaded ncpClientId={"14rlapbxs9"}>
+                  <NaverMap
+                    id={"map"}
+                    mapDivId={"react-naver-map"} // default name
+                    style={{ width: '100%', height: '100%' }}
+                    defaultCenter={{ lat: 37.481885, lng: 127.125866 }}
+                    defaultZoom={15}
+                  >
+                  
+                  <Marker 
+                    position={{ lat: 37.481885, lng: 127.125866 }}
+                    animation={Animation.BOUNCE}
+                    onClick={() => {
+                      alert('한식당 智')
+                    }}
+                  />
+                  </NaverMap>
+                </RenderAfterNavermapsLoaded>
+              </div>
+            </div>
           </section>
         </div>
       </main>
