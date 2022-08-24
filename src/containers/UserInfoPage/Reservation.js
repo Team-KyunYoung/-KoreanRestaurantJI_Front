ffe973a534294a90cfc3f47ba41b48c8541fff64
1g2g <wonji126326@gmail.com>
2022-08-24 18:55:44 +0900

    fix : reservation API

--- src/containers/UserInfoPage/Reservation.js
+++ src/containers/UserInfoPage/Reservation.js
@@ -13,92 +13,34 @@ function ReservationInnerPage(props) {
         ? "loading"
         : props.list.map((obj) => (
             <>
-              {props.date.toString() === obj.reservationDate.toString() ? (
-                <li key={obj.reservationNumber} className={styles.today}>
-                  <img src={image1} alt="reservation list" />
-                  <span className={styles.listSpan}>
-                    <span>{obj.reservationDate}</span>
-                    <span>|</span>
-                    <span>{obj.reservationTime}</span>
-                    <span>|</span>
-                    <span>{obj.reservationRoomName}</span>
-                    <br />
-                    <span>{obj.reservationName}</span>
-                    <span>|</span>
-                    <span>{obj.reservationHeadCount}</span>
-                    <span>|</span>
-                    <span>{obj.reservationPhoneNumber}</span>
-                  </span>
-                </li>
-              ) : (
-                <li key={obj.reservationNumber} className={styles.notToday}>
-                  <img src={image1} alt="reservation list" />
-                  <span className={styles.listSpan}>
-                    <span>{obj.reservationDate}</span>
-                    <span>|</span>
-                    <span>{obj.reservationTime}</span>
-                    <span>|</span>
-                    <span>{obj.reservationRoomName}</span>
-                    <br />
-                    <span>{obj.reservationName}</span>
-                    <span>|</span>
-                    <span>{obj.reservationHeadCount}</span>
-                    <span>|</span>
-                    <span>{obj.reservationPhoneNumber}</span>
-                  </span>
-                </li>
-              )}
+              <li
+                key={obj.reservationNumber}
+                className={
+                  props.date.toString() === obj.reservationDate.toString() //오늘 날짜에 스타일 부여(일단은 배경색)
+                    ? styles.today
+                    : styles.notToday
+                }
+              >
+                <img src={image1} alt="reservation list" />
+                <span className={styles.listSpan}>
+                  <span>{obj.reservationDate}</span>
+                  <span>|</span>
+                  <span>{obj.reservationTime}</span>
+                  <span>|</span>
+                  <span>{obj.reservationRoomName}</span>
+                  <br />
+                  <span>{obj.reservationName}</span>
+                  <span>|</span>
+                  <span>{obj.reservationHeadCount}</span>
+                  <span>|</span>
+                  <span>{obj.reservationPhoneNumber}</span>
+                </span>
+              </li>
               <hr />
             </>
           ))}
     </ul>
   );
-}
-function swap(left, right) {
-  var tmp = left;
-  left = right;
-  right = tmp;
-}
-function quick(list) {
-  console.log("quick");
-  // var pl = left;
-  // var pr = right;
-  // console.log(pl, pr);
-  // console.log((pl + pr) / 2);
-  // var idx = Math.floor((pl + pr) / 2);
-  // console.log(idx);
-  // var x = list[idx];
-  // console.log(x.reservationDate);
-  // console.log(pl, pr);
-  // do {
-  //   while (list[pl].reservationDate < x) pl++;
-  //   while (list[pr].reservationDate > x) pr--;
-  //   if (pl <= pr) {
-  //     swap(list[pl], list[pr]);
-  //     pl++;
-  //     pr--;
-  //   }
-  // } while (pl <= pr);
-  // if (pr > left) quick(list, left, pr);
-  // if (pl < right) quick(list, pl, right);
-  for (var i = 0; i < 11; i++) {
-    list.map((left) => {
-      var date1 = new Date(left.reservationDate);
-
-      console.log(" 1. " + date1);
-      list.map((right) => {
-        var date2 = new Date(right.reservationDate);
-        console.log("2.  " + date2 + " " + right.reservationNumber);
-        console.log(date1 === date2);
-        if (date1 >= date2) {
-          var tmp = left;
-          left = right;
-          right = tmp;
-          // console.log(left.reservationDate + " " + right.reservationDate);
-        }
-      });
-    });
-  }
 }
 const Reservation = () => {
   const [list, setList] = useState([]);
@@ -127,18 +69,16 @@ const Reservation = () => {
     UserService.findUser(); //유저를 찾는 과정이 있어야 아래 findReservation 통신 가능
   }, []);
   useEffect(() => {
-    ReservationService.findReservation().then((response) => {
-      setList(response.data.data);
-      console.log(response.data.data);
-      // quick(list);
-    });
-    // .catch(() => {
-    //   setLoading(true);
-    //   console.log("error:예약 목록 불러오기");
-    // });
+    ReservationService.findReservation()
+      .then((response) => {
+        setList(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch(() => {
+        setLoading(true);
+        console.log("error:예약 목록 불러오기");
+      });
   }, []);
-  quick(list);
-  //list 정렬
   return (
     <>
       <h2>Room I Booked</h2>
