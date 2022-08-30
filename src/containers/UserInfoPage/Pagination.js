ccc5100bb420780539528a592548b835d932ab24
1g2g <wonji126326@gmail.com>
2022-08-30 17:17:55 +0900

    fix : pagination의  setcurrentpage 에러

--- src/containers/UserInfoPage/Pagination.js
+++ src/containers/UserInfoPage/Pagination.js
@@ -58,7 +58,7 @@ const Page = ({ postsPerPage, totalPosts, currentPage, setCurrentPage }) => {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-caret-left-fill"
+            className="bi bi-caret-left-fill"
             viewBox="0 0 16 16"
           >
             <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
@@ -67,7 +67,6 @@ const Page = ({ postsPerPage, totalPosts, currentPage, setCurrentPage }) => {
         {pageNumbers.map((number) => (
           <Pagination.Item
             page={number}
-            setCurrentPage={setCurrentPage}
             onClick={() => setCurrentPage(number)}
             active={number === currentPage}
           >
