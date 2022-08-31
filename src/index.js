8dc6efc1ad472a5af910569a450679db67e84533
SShinMJ <82142527+SShinMJ@users.noreply.github.com>
2022-08-31 19:43:59 +0900

    Feat: Order(Modal) & Cart(API)
    * Feat: Cart & Order API
    
    * Merge&Fix: 'hover' Trigger Error
    
    * Feat: AddOrder & toCart With Modal
    
    * Feat: Cart find Response

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Modal from 'react-modal';
 import "./index.scss";
 import Home from "./containers/HomePage/Home";
 import Login from "./containers/LoginPage/Login";
@@ -56,3 +57,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+Modal.setAppElement('#root')
---
+++ src/lib/api/CartService.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+import Authentication from "lib/api/Authentication";
+
+const CART_API_BASE_URL = "/api/cart";
+
+class CartService {
+    addCartDish(dishNumber, cartQuantity){
+        let data = {
+            cartQuantity: cartQuantity,
+            dishNumber: dishNumber
+        }
+        Authentication.setupAxiosInterceptors();
+        return axios.post(CART_API_BASE_URL + "/create", JSON.stringify(data), {
+            headers: {
+              "Content-Type": `application/json`,
+            },
+        });
+    }
+
+    findCartByUser() {
+        Authentication.setupAxiosInterceptors();
+        return axios.get(CART_API_BASE_URL + "/find");
+    }
+
+    updateCartQuantitiy(cartNumber, cartQuantity) {
+        Authentication.setupAxiosInterceptors();
+        return axios.put(CART_API_BASE_URL + "/update/" + cartNumber, JSON.stringify(cartQuantity), {
+            headers: {
+              "Content-Type": `application/json`,
+            },
+        });
+    }
+
+    deleteCart(cartNumber) {
+        Authentication.setupAxiosInterceptors();
+        return axios.delete(CART_API_BASE_URL + "/delete" + cartNumber);
+    }
+}
+
+export default new CartService();
