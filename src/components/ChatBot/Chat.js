08d2003253f7f6a4d62a7b41a5930ab0b57147d5
SShinMJ <winny0678@gmail.com>
2022-09-07 16:17:44 +0900

    Fix: Error Fix, Feat: Additional question

--- src/components/ChatBot/Chat.js
+++ src/components/ChatBot/Chat.js
@@ -7,11 +7,19 @@ import { Link } from "react-router-dom";
 
 import Logo_Img from '../../assets/ChatBot/ChatBot_Logo.png';
 
+function ToLink(link) {
+  return(
+    <div className={styles.link_button}><Link to={link}>코스 페이지로 이동하기</Link></div>
+  )
+}
+
 // Set some properties of the bot
 const config = {
   botAvatar: Logo_Img,
   floating: true,
-  placeholder: '채팅이 불가능한 채널입니다.'
+  placeholder: ' 채팅이 불가능한 채널입니다.',
+  hideSubmitButton: true,
+  hideUserAvatar: true
 };
  
 // Creating our own theme
@@ -57,37 +65,31 @@ const steps = [
           { value: 7, label: '기타', trigger: 'etc' },
         ]
   }, {
-    id: 'Dish',
-    message: " 코스와 일반 메뉴 중 보고싶으신 것을 선택해주세요.",
-    trigger: 'Dish-select'
-}, {
-    id: 'Dish-select',
-    options: [
-        { value: 1, label: '코스 요리 확인', trigger: 'Dish-Course-link' },
-        { value: 2, label: '일반 메뉴 확인', trigger: 'Dish-link' },
-      ]
-}, {
-    id: 'Dish-Course-link',
-    component: (
-      <div className={styles.link_button}><Link to="/Course">코스 페이지로 이동하기</Link></div>
-    ),
-    end: true
-}, {
-    id: 'Dish-link',
-    component: (
-      <div className={styles.link_button}><Link to="/Dish">일반 메뉴 페이지로 이동하기</Link></div>
-    ),
-    end: true
-}, {
+      id: 'Dish',
+      message: " 코스와 일반 메뉴 중 보고싶으신 것을 선택해주세요.",
+      trigger: 'Dish-select'
+  }, {
+      id: 'Dish-select',
+      options: [
+          { value: 1, label: '코스 요리 확인', trigger: 'Dish-Course-link' },
+          { value: 2, label: '일반 메뉴 확인', trigger: 'Dish-link' },
+        ]
+  }, {
+      id: 'Dish-Course-link',
+      component: <ToLink link="/Course"/>,
+      trigger: 'reQuestion'
+  }, {
+      id: 'Dish-link',
+      component: <ToLink link="/Dish"/>,
+      trigger: 'reQuestion'
+  }, {
       id: 'Order',
       message: " 아래 버튼을 클릭하면 주문 페이지로 이동합니다. 이곳에서 주문을 완료해주세요.",
       trigger: 'Order-link'
   }, {
       id: 'Order-link',
-      component: (
-        <div className={styles.link_button}><Link to="/Order">주문 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/Order"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'Reservation',
       message: " 아래 페이지에서 예약을 진행하실 수 있습니다.",
@@ -98,20 +100,16 @@ const steps = [
       trigger: 'Reservation-link'
   }, {
       id: 'Reservation-link',
-      component: (
-        <div className={styles.link_button}><Link to="/SelectRoom">예약 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/SelectRoom"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'Map',
       message: " 아래 링크에서 식당의 정확한 위치정보를 네이버 지도로 확인하실 수 있습니다.",
       trigger: 'Map-link'
   }, {
       id: 'Map-link',
-      component: (
-        <div className={styles.link_button}><Link to="/Map">지도 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/Map"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'UserInfo',
       message: " 어떤 것을 확인하시고 싶으신가요?",
@@ -124,16 +122,12 @@ const steps = [
         ]
   }, {
       id: 'UserInfo-Order-link',
-      component: (
-        <div className={styles.link_button}><Link to="/UserInfo/ordered">주문 확인 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/UserInfo/ordered"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'UserInfo-Reservation-link',
-      component: (
-        <div className={styles.link_button}><Link to="/UserInfo/reservation">예약 확인 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/UserInfo/reservation"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'QNA',
       message: " 문의 목록을 확인하고 싶으신가요? 아니면 문의를 하고 싶으신가요?",
@@ -150,20 +144,16 @@ const steps = [
       trigger: 'QNA-Board-link'
   }, {
       id: 'QNA-Board-link',
-      component: (
-        <div className={styles.link_button}><Link to="/QnA">문의 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/QnA"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'QNA-Post',
       message: " 아래 버튼을 눌러 문의를 작성하실 수 있습니다.",
       trigger: 'QNA-Post-link'
   }, {
       id: 'QNA-Post-link',
-      component: (
-        <div className={styles.link_button}><Link to="/QnA">문의 작성 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/QnA"/>,
+      trigger: 'reQuestion'
   }, {
       id: 'etc',
       message: " 그 밖의 문의 사항은 FAQ를 확인해 주시고, 관련 문의 사항이 없다면 QNA 페이지에 문의 남겨주세요.",
@@ -176,10 +166,17 @@ const steps = [
       ]
   }, {
       id: 'FAQ-Board',
-      component: (
-        <div className={styles.link_button}><Link to="/FAQ">FAQ 페이지로 이동하기</Link></div>
-      ),
-      end: true
+      component: <ToLink link="/FAQ"/>,
+      trigger: 'reQuestion_select'
+  }, {
+      id: 'reQuestion',
+      message: '더 궁금하신 사항이 있으시면 버튼을 눌러주세요',
+      trigger: 'reQuestion_select'
+  }, {
+      id: 'reQuestion_select',
+      options: [
+          { value: 1, label: '더 질문하기', trigger: '2' }
+      ]
   }
 ];
 
@@ -190,7 +187,7 @@ const Chat = () => {
         floatingStyle= {{}}
         bubbleOptionStyle= {{ backgroundColor: "#f5f8fb", color: "#424566", border: '2px solid #424566', 
                               hover: {backgroundColor: "#424566", color: "#fff"} }}   //hover 적용 안됨. 하는 법 모르겠음
-        customStyle= {{ border: '2px solid #424566' }}
+        customStyle= {{ width: '250px', border: '2px solid #424566', transform: 'translate(19%, 0%)' }}
         headerTitle = '&ensp; 智 문의하기'
         steps={steps}
         {...config}
