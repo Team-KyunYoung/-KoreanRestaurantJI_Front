0ef46fd93ab017dd810cbf16e22304d77bce9f20
SShinMJ <winny0678@gmail.com>
2022-09-08 18:26:12 +0900

    Feat: Add Login valid time(Automatic logout)

--- src/lib/api/Authentication.js
+++ src/lib/api/Authentication.js
@@ -3,14 +3,20 @@ import axios from "axios";
 class Authentication {
   loginTokenSave(token) {
     console.log("===login Token Save===")
-    localStorage.setItem('token', token)
+    //로컬 스토리지에 유효시간(6시간) 추가하여 저장.
+    const item = {
+      value: token,
+      expire: Date.now() + 21600000,
+    } 
+    localStorage.setItem('token', JSON.stringify(item))
     this.setupAxiosInterceptors()
   }
 
   setupAxiosInterceptors() {
     axios.interceptors.request.use(
       config => {
-        const token = localStorage.getItem('token');
+        const obj = localStorage.getItem('token');
+        const token = JSON.parse(obj).value;
         if (token) {
           config.headers['X-AUTH-TOKEN'] = token;
           //X-AUTH-TOKEN으로 다른 url 이용할 때 헤더로 넣어줘야한다.
@@ -23,11 +29,18 @@ class Authentication {
   }
 
   isUserLoggedIn() {
-    const token = localStorage.getItem('token');
+    const localStorageData = localStorage.getItem('token');
     console.log("===UserloggedInCheck===");
-    console.log(token);
 
-    if(token) {
+    if(localStorageData) {
+      // 현재 시간과 localStorage의 expire 시간 비교
+      if( Date.now() > JSON.parse(localStorageData).expire) {    
+        console.log("로그인 유효시간이 지나 로그아웃 됩니다.")
+        // 만료시간이 지난 item 삭제    
+        localStorage.removeItem('token');
+        return false;
+      }
+      console.log(JSON.parse(localStorageData).value);
       return true;
     }
     return false;
