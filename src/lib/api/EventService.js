ce4e78546a92b4e53634c78a64ef53f4d3d4a6e9
1g2g <wonji126326@gmail.com>
2022-09-10 22:25:20 +0900

    feat : event페이지 생성 & 조회, 삭제 api

---
+++ src/lib/api/EventService.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import Authentication from "./Authentication";
+const USER_API_BASE_URL = "/api/event";
+class EventService {
+  createEvent(eventContents, eventImage, eventTitle) {
+    let data = {
+      eventContents: eventContents,
+      eventImage: eventImage,
+      eventTitle: eventTitle,
+    };
+    Authentication.setupAxiosInterceptors();
+    return axios.post(
+      USER_API_BASE_URL + "/create/event",
+      JSON.stringify(data),
+      {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      }
+    );
+  }
+  deleteEvent(eventNumber) {
+    Authentication.setupAxiosInterceptors();
+    return axios.delete(
+      USER_API_BASE_URL + "/delete/" + eventNumber,
+      JSON.stringify(),
+      {
+        headers: { "Content-Type": `application/json` },
+      }
+    );
+  }
+  findEvent(eventNumber) {
+    Authentication.setupAxiosInterceptors();
+    return axios.get(
+      USER_API_BASE_URL + "/find/" + eventNumber,
+      JSON.stringify(),
+      {
+        headers: { "Content-Type": `application/json` },
+      }
+    );
+  }
+  findAllEvent() {
+    return axios.get(USER_API_BASE_URL + "/find/event", JSON.stringify(), {
+      headers: { "Content-Type": `application/json` },
+    });
+  }
+  updateEvent(eventNumber, eventContents, eventImage, eventTitle) {
+    let data = {
+      eventContents: eventContents,
+      eventImage: eventImage,
+      eventTitle: eventTitle,
+    };
+    Authentication.setupAxiosInterceptors();
+    return axios.put(
+      USER_API_BASE_URL + "/update/qna/" + eventNumber,
+      JSON.stringify(data),
+      {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      }
+    );
+  }
+}
+export default new EventService();
