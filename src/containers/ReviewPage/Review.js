2f1e56a4235a7d653fc062f2bd1a8a5a6c8341c5
SShinMJ <winny0678@gmail.com>
2022-09-11 17:51:46 +0900

    Fead: Reivew Api

--- src/containers/ReviewPage/Review.js
+++ src/containers/ReviewPage/Review.js
@@ -1,58 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./Review.module.scss";
 import Header from "components/header/Header";
 import Chat from "../../components/ChatBot/Chat";
 import Footer from "components/footer/Footer";
+import ReviewService from "lib/api/ReviewService";
+import ClassNameSelect from './ClassNameSelect';
 
 const image1 = "https://picsum.photos/600/600";
 const image2 = "https://picsum.photos/300/600";
 const image3 = "https://picsum.photos/600/300";
 const image4 = "https://picsum.photos/300/300";
+function imageSelect(num){
+  if(num === 1) return image1
+  else if(num === 2) return image2
+  else if(num === 3) return image3
+  else return image4
+}
 
 const Review = () => {
+  let cnt = 0;
+  function ReviewContent({content, image}){
+    return (
+      <div className={`${styles.review} ${ClassNameSelect(++cnt)}`}>
+        <div className={styles.imgBox}>
+          {/* <img src={image}></img> */}
+          <img src={imageSelect(cnt%4+1)}></img>
+        </div>
+        <div className={styles.contents}>
+          <p>{content}</p>
+        </div>
+      </div>
+    )
+  }
+
+  const [reviewList, setReviewList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    ReviewService.findAllReview()
+      .then((response) => {
+        setReviewList(response.data.data.slice(0, 11))
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+
   return (
     <div id={styles.ReviewPage}>
       <Header />
       <main className={styles.container}>
-        <div className={`${styles.review} ${styles.content1}`}>
-          <div className={styles.imgBox}>
-            <img src={image1}></img>
-          </div>
-          <div className={styles.contents}>
-            <p>최고의 맛</p>
-          </div>
-        </div>
-        <div className={`${styles.review} ${styles.content2}`}>
-            <img src={image4}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content3}`}>
-            <img src={image3}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content4}`}>
-            <img src={image2}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content5}`}>
-            <img src={image1}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content6}`}>
-            <img src={image4}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content7}`}>
-            <img src={image4}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content8}`}>
-            <img src={image2}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content9}`}>
-            <img src={image1}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content10}`}>
-            <img src={image4}></img>
-        </div>
-        <div className={`${styles.review} ${styles.content11}`}>
-            <img src={image3}></img>
-        </div>
+        { isLoading ? "Loading..." :
+          reviewList.length == 0 ? <div className={styles.ReviewNone}>리뷰가 없습니다.</div>
+          : reviewList.map( review => (
+            <ReviewContent key={review.reviewContents}
+              content={review.reviewContents}
+              image={review.reviewImage}
+            />
+          ))
+        }
       </main>
       <Chat />
       <Footer />
