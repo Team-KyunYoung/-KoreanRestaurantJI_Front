9223ed85f0d05d9911596c0cfecaf83363d40252
1g2g <wonji126326@gmail.com>
2022-09-17 16:32:57 +0900

    fix : arrow  error

--- src/containers/UserInfoPage/Reservation.js
+++ src/containers/UserInfoPage/Reservation.js
@@ -121,41 +121,43 @@ const Reservation = (props) => {
             className={styles.pagination}
           ></Page>
         </div>
-        {param.mode === "now" ? (
-          <Link to="/UserInfo/reservation/past">
-            <span>지난 예약</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="64"
-              fill="currentColor"
-              className="bi bi-chevron-compact-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
-              />
-            </svg>
-          </Link>
-        ) : (
-          <Link to="/UserInfo/reservation/now">
-            <span>현재 예약</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="64"
-              fill="currentColor"
-              className="bi bi-chevron-compact-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
-              />
-            </svg>
-          </Link>
-        )}
+        <div className={styles.goList}>
+          {param.mode === "now" ? (
+            <Link to="/UserInfo/reservation/past" className={styles.goPast}>
+              <span>지난 예약</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="currentColor"
+                className="bi bi-chevron-compact-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
+                />
+              </svg>
+            </Link>
+          ) : (
+            <Link to="/UserInfo/reservation/now" className={styles.goNow}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="currentColor"
+                class="bi bi-chevron-compact-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"
+                />
+              </svg>
+              <span>현재 예약</span>
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
