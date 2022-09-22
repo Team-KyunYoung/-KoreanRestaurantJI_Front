8b4d0cee6aafc6019445e4858f16d79a197b6804
1g2g <wonji126326@gmail.com>
2022-09-22 23:50:33 +0900

    style & fix : event page

--- src/containers/EventPage/EventPost.js
+++ src/containers/EventPage/EventPost.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import MediaQuery from "react-responsive";
 import Header from "components/header/Header";
 import Footer from "components/footer/Footer";
 import Chat from "../../components/ChatBot/Chat";
@@ -18,7 +19,6 @@ const EventPost = () => {
   const navigate = useNavigate();
   console.log(param.number);
   useEffect(() => {
-    alert(param.number);
     EventService.findEvent(Number(param.number))
       .then((response) => {
         console.log(response);
@@ -50,42 +50,62 @@ const EventPost = () => {
     <div id="EventPage">
       <Header />
       <main className={styles.container}>
-        <ImgBanner
-          img={image1}
-          pageTitle="Event"
-          pageDetails="Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry."
-        />
-        <section className={styles.eventBox}>
-          {isLoading ? (
-            ""
-          ) : (
-            <div className={styles.eventPost}>
-              <h1>{list.eventTitle}</h1>
-              <p>
-                {list.writeDate}
-                {list.eventView}
-              </p>
-              {/*<img src={list.eventImage} alt={list.eventTitle} />*/}
+        <div>
+          <header className={styles.title}></header>
+          <section className={styles.main}>
+            <div className={styles.banner}>
               <img src={image1} alt={list.eventTitle} />
-              <p>{list.eventContents}</p>
+              {/*<img src={list.eventImage} alt={list.eventTitle} />*/}
             </div>
-          )}
-        </section>
-        <section className={styles.eventBox}>
-          {isAdmin ? (
-            <div className={styles.btn}>
-              <button type="submit" onClick={updateEventPost}>
-                수정하기
-              </button>
-              <button type="button" onClick={deleteEventPost}>
-                삭제하기
-              </button>
+            <div className={styles.contents}>
+              <MediaQuery minWidth={993}>
+                <h2>Event Post</h2>
+              </MediaQuery>
+              <div className={styles.editProfile}>
+                <h1>{list.eventTitle}</h1>
+                <div>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-calendar"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>{" "}
+                    {list.writeDate}
+                  </span>
+                  {"     "}
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-eye"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>{" "}
+                    {list.eventView}
+                  </span>
+                </div>
+                <p className={styles.details}>{list.eventContents}</p>
+              </div>
+              <div className={styles.btn}>
+                <button type="button" onClick={updateEventPost}>
+                  수정하기
+                </button>
+                <button type="button" onClick={deleteEventPost}>
+                  삭제하기
+                </button>
+              </div>
             </div>
-          ) : (
-            ""
-          )}
-        </section>
+          </section>
+        </div>
       </main>
       <Chat />
       <Footer />
