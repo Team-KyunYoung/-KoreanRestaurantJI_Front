93a7518bfb69aab71a0abccf58106c54c93177a4
SShinMJ <winny0678@gmail.com>
2022-09-22 20:07:33 +0900

    Feat: DishSetting data processing

--- src/containers/AdminPage/DishSetting.js
+++ src/containers/AdminPage/DishSetting.js
@@ -1,9 +1,97 @@
+import DishService from "lib/api/DishService";
 import React, { useState } from "react";
 
 import styles from "./Admin.module.scss";
 import MenuBar from "./MenuBar";
 
 const DishSetting = () => {
+  const [createDishInput, setCreateDishInput] = useState({
+    dishName: "",
+    dishPhoto: "",
+    dishPrice: "",
+    dishCategory: "",
+    dishDescription: "",
+    dishCalroies: "",
+    dishCarbohydrate: "",
+    dishCholesterol: "",
+    dishFat: "",
+    dishProtein: "",
+    dishServingSize: "",
+    dishSodium: "",
+    dishSugars: "",
+    dishTransFat: "",
+  });
+  const handleCreateDishInput = (e)=>{
+    setCreateDishInput({
+      ...createDishInput,
+      [e.target.name] : e.target.value,
+    });
+  }
+  function createDishBtn() {
+    DishService.createDish(createDishInput)
+    .then(() => {
+      alert("요리 정보가 등록되었습니다.")
+      setCreateDishInput({
+        dishName: "",
+        dishPhoto: "",
+        dishPrice: "",
+        dishCategory: "전식",
+        dishDescription: "",
+        dishCalroies: "",
+        dishCarbohydrate: "",
+        dishCholesterol: "",
+        dishFat: "",
+        dishProtein: "",
+        dishServingSize: "",
+        dishSodium: "",
+        dishSugars: "",
+        dishTransFat: "",
+      })
+    })
+    .catch((error) => {
+      console.log(error.response);
+      alert("요리 등록에 실패하였습니다.")
+    });
+  }
+
+  const [searchDishInput, setSearchDishInput] = useState("");
+  const handleSearchDishInput = (e)=>{  setSearchDishInput(e.target.value);  }
+  const [searchDishList, setSearchDishList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  function searchDishBtn() {
+    setSearchDishList([])
+    DishService.searchDish(searchDishInput)
+    .then((response) => {
+      setSearchDishInput("")
+      console.log(response.data.data)
+      setSearchDishList(response.data.data)
+      setIsLoading(false)
+    })
+    .catch((error) => {
+      setIsLoading(false)
+      console.log(error.response);
+    });
+  }
+  function deleteDishBtn(dishNumber) {
+    DishService.deleteDish(dishNumber)
+    .then((response) => {
+      alert("해당 요리 데이터가 삭제되었습니다.")
+      window.location.reload()
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+  }
+  function SearchOutput({dishNumber, dishName}){
+    console.log(dishNumber + ", " + dishName)
+    return(
+      <div className={styles.searchDishList}>
+        <span>{dishNumber}</span>
+        <span>{dishName}</span>
+        <button onClick={() => deleteDishBtn(dishNumber)}>삭제</button>
+      </div>
+    )
+  }
 
   return (
     <div className="DishSettingPage">
@@ -14,28 +102,52 @@ const DishSetting = () => {
               <div className={styles.title}><h3>단품 요리 설정 페이지</h3></div>
               <div className={styles.settingContents}>
                 <div className={styles.create}>
-                    <h3>단품 요리 등록하기</h3>
+                    <h3>단품 요리 등록하기</h3><hr/>
                     <div className={styles.createInput}>
-                      <label>요리 이름 <input></input></label><br/>
-                      <label>요리 사진 url <input></input></label><br/>
-                      <label>요리 가격 <input></input></label><br/>
-                      <label>요리 상세 설명 <input></input></label><br/>
-                      <label>요리 분류 
-                        <select>
-                          <option>전식</option>
-                          <option>중식</option>
-                          <option>후식</option>
-                        </select>
-                      </label><br/>
-                      <button>등록</button>
+                      <div className={styles.dishInput}>
+                        <h4>요리 정보</h4>
+                        <label>요리 이름 <input name="dishName" onChange={handleCreateDishInput} value={createDishInput.dishName}/></label><br/>
+                        <label>요리 사진 url <input name="dishPhoto" onChange={handleCreateDishInput} value={createDishInput.dishPhoto}/></label><br/>
+                        <label>요리 가격 <input name="dishPrice" onChange={handleCreateDishInput} value={createDishInput.dishPrice} placeholder="숫자만 입력"/></label><br/>
+                        <label>요리 상세 설명 <input name="dishDescription" onChange={handleCreateDishInput} value={createDishInput.dishDescription} placeholder="230자 이내로 입력"/></label><br/>
+                        <label>요리 분류 
+                          <select name="dishCategory" onChange={handleCreateDishInput} value={createDishInput.dishCategory}>
+                            <option key="전식" value="전식">전식</option>
+                            <option key="중식" value="중식">중식</option>
+                            <option key="후식" value="후식">후식</option>
+                          </select>
+                        </label><br/>
+                      </div>
+                      <div className={styles.NutritionFactsInput}>
+                        <h4>요리 영양 성분 정보</h4>
+                        <label>용량(g) <input name="dishServingSize" onChange={handleCreateDishInput} value={createDishInput.dishServingSize} placeholder="숫자만 입력"/></label><br/>
+                        <label>칼로리(kcal) <input name="dishCalroies" onChange={handleCreateDishInput} value={createDishInput.dishCalroies} placeholder="숫자만 입력"/></label><br/>
+                        <label>탄수화물(g) <input name="dishCarbohydrate" onChange={handleCreateDishInput} value={createDishInput.dishCarbohydrate} placeholder="숫자만 입력"/></label><br/>
+                        <label>단백질(g) <input name="dishProtein" onChange={handleCreateDishInput} value={createDishInput.dishProtein} placeholder="숫자만 입력"/></label><br/>
+                        <label>지방(g) <input name="dishFat" onChange={handleCreateDishInput} value={createDishInput.dishFat} placeholder="숫자만 입력"/></label><br/>
+                        <label>콜레스테롤(g) <input name="dishCholesterol" onChange={handleCreateDishInput} value={createDishInput.dishCholesterol} placeholder="숫자만 입력"/></label><br/>
+                        <label>트랜스지방(g) <input name="dishTransFat" onChange={handleCreateDishInput} value={createDishInput.dishTransFat} placeholder="숫자만 입력"/></label><br/>
+                        <label>당(g) <input name="dishSugars" onChange={handleCreateDishInput} value={createDishInput.dishSugars} placeholder="숫자만 입력"/></label><br/>
+                        <label>나트륨(g) <input name="dishSodium" onChange={handleCreateDishInput} value={createDishInput.dishSodium} placeholder="숫자만 입력"/></label><br/>
+                      </div>
+                      <button onClick={createDishBtn}>등록</button>
                     </div>
                 </div>
                 <hr/>
                 <div className={styles.delete}>
                     <h3>단품 요리 삭제하기</h3>
                     <div className={styles.deleteSearch}>
-                      <label>요리 이름 <input></input></label>
-                      <button>검색</button>
+                      <label>요리 이름 <input name="searchName" onChange={handleSearchDishInput} value={searchDishInput}/></label>
+                      <button onClick={searchDishBtn}>검색</button><br/>
+                      { isLoading ? null :
+                        searchDishList.length == 0 ? <div className={styles.searchNone}>검색어가 포함된 요리가 없습니다.</div>
+                        : searchDishList.map( searchData => (
+                          <SearchOutput key={searchData.dishNumber}
+                            dishNumber={searchData.dishNumber}
+                            dishName={searchData.dishName}
+                          />
+                        ))
+                      }
                     </div>
                 </div>
               </div>
--- src/lib/api/DishService.js
+++ src/lib/api/DishService.js
@@ -5,25 +5,23 @@ import Authentication from "lib/api/Authentication";
 const DISH_API_BASE_URL = "/api/dish";
 
 class DishService {
-  createDish(dishName, dishPhoto, dishPrice, dishCategory, dishDescription,
-    dishCalroies, dishCarbohydrate, dishCholesterol, dishFat, dishProtein, dishServingSize,
-    dishSodium, dishSugars, dishTransFat) {
+  createDish(createDishInput) {
     let data = {
-      dishName: dishName,
-      dishPhoto: dishPhoto,
-      dishPrice: dishPrice,
-      dishCategory: dishCategory,
-      dishDescription: dishDescription,
+      dishName: createDishInput.dishName,
+      dishPhoto: createDishInput.dishPhoto,
+      dishPrice: createDishInput.dishPrice,
+      dishCategory: createDishInput.dishCategory,
+      dishDescription: createDishInput.dishDescription,
       nutritionFacts: {
-        dishCalroies: dishCalroies,
-        dishCarbohydrate: dishCarbohydrate,
-        dishCholesterol: dishCholesterol,
-        dishFat: dishFat,
-        dishProtein: dishProtein,
-        dishServingSize: dishServingSize,
-        dishSodium: dishSodium,
-        dishSugars: dishSugars,
-        dishTransFat: dishTransFat
+        dishCalroies: createDishInput.dishCalroies,
+        dishCarbohydrate: createDishInput.dishCarbohydrate,
+        dishCholesterol: createDishInput.dishCholesterol,
+        dishFat: createDishInput.dishFat,
+        dishProtein: createDishInput.dishProtein,
+        dishServingSize: createDishInput.dishServingSize,
+        dishSodium: createDishInput.dishSodium,
+        dishSugars: createDishInput.dishSugars,
+        dishTransFat: createDishInput.dishTransFat
       }
     };
     Authentication.setupAxiosInterceptors();
