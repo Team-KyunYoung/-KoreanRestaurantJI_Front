0aa11e8cb19c4aa80eae1eeb837d2459cd42647e
1g2g <87280835+1g2g@users.noreply.github.com>
2022-10-26 15:30:33 +0900

    Revert "version 1.0.0"

--- README.md
+++ README.md
@@ -1,73 +1,7 @@
-#	:stew: [FrontEnd]Korean Restaurant 智
-기본 url : http://localhost:3030/
+[FrontEnd]Korean-Restaurant-JI
 
-<br>
+In the project directory, you can run:
+### `npm start`
 
-## :point_right: Info
-한식 전문점 '智' 웹사이트의 FrontEnd 프로젝트입니다.
-
-'智'는 제작자들의 이름에서 따온 임의의 레스토랑으로 본 웹사이트는 실제로 레스토랑에서 제공하면 좋을 서비스들을 가정해보고 구현해보았습니다.
-
-<br>
-
-## :pushpin: Feature
-\#UserInteractive \#UseFamiliar
-- Responsive Web Design
-- Try to apply various third party library 
-  - react-bootstrap
-  - react-naver-maps
-  - react-simple-chatbot
-  - etc
-- Provide Various Service 
-    - Merbership Service: Sign Up, Cancellation, Edit Profile
-    - chat : 
-    - Reservation : Make, Cancel or Change a reservation
-    - to-go : Place or Cancel an order
-    - review 
-    - qna & faq : Can view left by others or ask questions (Anonymity is possible) 
-    - etc
-
-<br>
-
-## :hammer_and_wrench: Tech Stack
-<div>
-<img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=skyblue" />
-<img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white" />
-<img src="https://img.shields.io/badge/SCSS-CC6699?style=flat-square&logo=SASS&logoColor=white" />
-<img src="https://img.shields.io/badge/BootStrap-7952B3?style=flat-square&logo=BootStrap&logoColor=white" />
-<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white" />
-<img src="https://img.shields.io/badge/jQuery-0769AD?style=flat-square&logo=jQuery&logoColor=white" />
-</div>
-
-<br>
-
-## :gear: Development Environment
-![Development Environment Front](https://user-images.githubusercontent.com/82142527/179345375-3e4c5db7-8ca4-4bf3-a8df-51cc24be0c03.png)
-
-<br>
-
-## :desktop_computer: UI/UX
-### Header
-![image](https://user-images.githubusercontent.com/87280835/197722796-4f223cfb-bf6a-40b3-ae84-fb4defe53360.png)
-
-### Footer
-![image](https://user-images.githubusercontent.com/87280835/197723011-13412fdc-928f-4b14-94b0-a3085b14a168.png)  
-### MainPage
-![home](https://user-images.githubusercontent.com/87280835/197722005-a7881193-215d-4cf1-b2cf-1ed1e711a0f5.png)
-### Introduction Page
-![localhost_3001_Info (1)](https://user-images.githubusercontent.com/87280835/197721536-79f125ea-1043-43b5-9d28-e0c0ca15bb33.png)
-### Map Page
-![localhost_3001_Map](https://user-images.githubusercontent.com/87280835/197723460-9455f228-4dea-423d-ac91-12b110726548.png)
-### Togo Service
-#### 
-
-### Hall Reservation Service
-#### Book Page
-![localhost_3001_SelectRoom](https://user-images.githubusercontent.com/87280835/197724179-c5df4455-cb4f-4d5f-8473-9dfe3e73cca8.png)
-
-### Merber Service
-#### LoginPage
-![image](https://user-images.githubusercontent.com/87280835/197719418-fdd9bb82-9cf6-4417-8028-d2e8a616bc53.png)
-#### Member Page
-### Chating Service
-![image](https://user-images.githubusercontent.com/87280835/197725346-f46416a1-1633-431b-9ebc-6e930767b9ce.png)
+Runs the app in the development mode.\
+Open [http://localhost:3000]
--- src/index.js
+++ src/index.js
@@ -1,87 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Modal from "react-modal";
-import "./index.scss";
-
-import PublicRoute from "components/LimitRoute/PublicRoute";
-import PrivateRoute from "components/LimitRoute/PrivateRoute";
-import AdminRoute from "components/LimitRoute/AdminRoute";
-
-import Home from "containers/HomePage/Home";
-import Login from "containers/LoginPage/Login";
-import FindPassword from "containers/LoginPage/FindPassword";
-import Signup from "containers/SignupPage/Signup";
-import Info from "containers/InfoPage/Info";
-import Map from "containers/MapPage/Map";
-import Cart from "containers/CartPage/Cart";
-import Course from "containers/CoursePage/Course";
-import Order from "containers/OrderPage/Order";
-import SelectRoom from "containers/BookingPage/SelectRoom";
-import SelectMore from "containers/BookingPage/SelectMore";
-import Review from "containers/ReviewPage/Review";
-import Dish from "containers/DishPage/Dish";
-import DishDetails from "containers/DishPage/DishDetails";
-import UserInfo from "containers/UserInfoPage/UserInfo";
-import FAQBoard from "containers/CustomerServicePage/FAQBoard";
-import CreateFAQ from "containers/CustomerServicePage/CreateFAQ";
-import QnABoard from "containers/CustomerServicePage/QnABoard";
-import QnAPost from "containers/CustomerServicePage/QnAPost";
-import CreateQnA from "containers/CustomerServicePage/CreateQnA";
-import EventBoard from "containers/EventPage/EventBoard";
-import EventPost from "containers/EventPage/EventPost";
-import CreateEvent from "containers/EventPage/CreateEvent";
-import reportWebVitals from "reportWebVitals";
-import AdminHome from "containers/AdminPage/Home";
-import DishSetting from "containers/AdminPage/DishSetting";
-import CourseSetting from "containers/AdminPage/CourseSetting";
-import RoomSetting from "containers/AdminPage/RoomSetting";
-import ReservationSetting from "containers/AdminPage/ReservationSetting";
-import OrderSetting from "containers/AdminPage/OrderSetting";
-import NotFound from "containers/NotFoundPage/NotFound";
-import "bootstrap/dist/css/bootstrap.css"; //부트스트랩 : 삭제 금지
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="Login" element={<PublicRoute element={<Login />}/>} />
-      <Route path="FindPassword" element={<PublicRoute element={<FindPassword />}/>} />
-      <Route path="Signup" element={<PublicRoute element={<Signup />}/>} />
-      <Route path="Info" element={<Info />} />
-      <Route path="Map" element={<Map />} />
-      <Route path="Cart" element={<PrivateRoute element={<Cart />}/>} />
-      <Route path="Course" element={<Course />} />
-      <Route path="Order" element={<Order />} />
-      <Route path="SelectRoom" element={<SelectRoom />} />
-      <Route path="SelectMore/:roomNumber/:roomName" element={<PrivateRoute element={<SelectMore />}/>} />
-      <Route path="Review" element={<Review />} />
-      <Route path="Dish" element={<Dish />} />
-      <Route path="Dish/:dishNumber/:dishName" element={<DishDetails />} />
-      <Route path="UserInfo/:location/:mode" element={<PrivateRoute element={<UserInfo />}/>} />
-      <Route path="FAQBoard" element={<FAQBoard />} />
-      <Route path="FAQBoard/:mode/:number" element={<CreateFAQ />} />
-      <Route path="QnABoard" element={<QnABoard />} />
-      <Route path="QnABoard/:number/:isPrivate" element={<QnAPost />} />
-      <Route path="CreateQnA" element={<PrivateRoute element={<CreateQnA />}/>} />
-      <Route path="Event" element={<EventBoard />} />
-      <Route path="Event/:mode/:number" element={<CreateEvent />} />
-      <Route path="Event/Post/:number" element={<EventPost />} />
-      <Route path="Admin/Home" element={<AdminRoute element={<AdminHome />}/>} />
-      <Route path="Admin/DishSetting" element={<AdminRoute element={<DishSetting />}/>} />
-      <Route path="Admin/CourseSetting" element={<AdminRoute element={<CourseSetting />}/>} />
-      <Route path="Admin/RoomSetting" element={<AdminRoute element={<RoomSetting />}/>} />
-      <Route path="Admin/ReservationSetting" element={<AdminRoute element={<ReservationSetting />}/>} />
-      <Route path="Admin/OrderSetting" element={<AdminRoute element={<OrderSetting />}/>} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-Modal.setAppElement("#root");
