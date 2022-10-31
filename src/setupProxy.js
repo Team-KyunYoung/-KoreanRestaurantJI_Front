64727c10c165b6fe88debfb521854af0ed0fe8f3
SShinMJ <winny0678@gmail.com>
2022-10-31 22:03:15 +0900

    Fix: BackEnd proxy for CORS

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.use(
   '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://3.39.232.96:8080',
       changeOrigin: true,
     })
   );
