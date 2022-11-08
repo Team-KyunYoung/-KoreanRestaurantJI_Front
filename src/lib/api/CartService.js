4ec7be3c4ce81723afdc9e4c80d5c7a5a7641754
1g2g <wonji126326@gmail.com>
2022-11-08 14:50:40 +0900

    fix : console error

--- src/lib/api/CartService.js
+++ src/lib/api/CartService.js
@@ -5,37 +5,41 @@ import Authentication from "lib/api/Authentication";
 const CART_API_BASE_URL = "https://www.koreanrestaurantji.ga/api/cart";
 
 class CartService {
-    addCartDish(dishNumber, cartQuantity){
-        let data = {
-            cartQuantity: cartQuantity,
-            dishNumber: dishNumber
-        }
-        Authentication.setupAxiosInterceptors();
-        return axios.post(CART_API_BASE_URL + "/create", JSON.stringify(data), {
-            headers: {
-              "Content-Type": `application/json`,
-            },
-        });
-    }
+  addCartDish(dishNumber, cartQuantity) {
+    let data = {
+      cartQuantity: cartQuantity,
+      dishNumber: dishNumber,
+    };
+    Authentication.setupAxiosInterceptors();
+    return axios.post(CART_API_BASE_URL + "/create", JSON.stringify(data), {
+      headers: {
+        "Content-Type": `application/json`,
+      },
+    });
+  }
 
-    findCartByUser() {
-        Authentication.setupAxiosInterceptors();
-        return axios.get(CART_API_BASE_URL + "/find");
-    }
+  findCartByUser() {
+    Authentication.setupAxiosInterceptors();
+    return axios.get(CART_API_BASE_URL + "/find");
+  }
 
-    updateCartQuantitiy(cartNumber, cartQuantity) {
-        Authentication.setupAxiosInterceptors();
-        return axios.put(CART_API_BASE_URL + "/update/" + cartNumber, JSON.stringify(cartQuantity), {
-            headers: {
-              "Content-Type": `application/json`,
-            },
-        });
-    }
+  updateCartQuantitiy(cartNumber, cartQuantity) {
+    Authentication.setupAxiosInterceptors();
+    return axios.put(
+      CART_API_BASE_URL + "/update/" + cartNumber,
+      JSON.stringify(cartQuantity),
+      {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      }
+    );
+  }
 
-    deleteCart(cartNumber) {
-        Authentication.setupAxiosInterceptors();
-        return axios.delete(CART_API_BASE_URL + "/delete" + cartNumber);
-    }
+  deleteCart(cartNumber) {
+    Authentication.setupAxiosInterceptors();
+    return axios.delete(CART_API_BASE_URL + "/delete" + cartNumber);
+  }
 }
 
 export default new CartService();
